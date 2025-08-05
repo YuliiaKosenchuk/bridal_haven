7be06d95089b07edb70650eae8dc2c8591759142
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2025-08-05 13:50:37 +0200

    add product

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,10 @@
-console.log('test');
+const swiper = new Swiper(".accessories__slider", {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  loop: true,
+
+  navigation: {
+    nextEl: ".arrow-next",
+    prevEl: ".arrow-prev",
+  },
+});
